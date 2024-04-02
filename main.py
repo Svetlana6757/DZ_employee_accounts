93559a2cda5a769136b3ea1f62a09150fe27d5ea
Svetlana6757 <163926856+Svetlana6757@users.noreply.github.com>
2024-04-02 20:22:15 +0300

    add def get

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ class User:
         self._user_id = user_id
         self._name = name
         self._access_level = access_level
+
+    def get_user_id(self):
+        return self._user_id
+
+    def get_name(self):
+        return self._name
+
+    def get_access_level(self):
+        return self._access_level
+
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def __str__(self):
+        return f"ID: {self._user_id}, Name: {self._name}, Access Level: {self._access_level}"
+
